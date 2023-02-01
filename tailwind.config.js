227ad3fbb671dc9b62ed6201b102aae7f98f5c32
Alejandro Krasovsky <alejandrok93@gmail.com>
2023-02-01 13:50:19 -0800

    Add product detail page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        '[auto,auto,1fr]': 'auto auto 1fr',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // ...
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
