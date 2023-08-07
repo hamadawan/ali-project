19bf54860fbd4e913bea7a2c988e916d1ecd0e14
Alejandro Krasovsky <alejandrok93@gmail.com>
2023-08-06 22:11:39 -0700

    Same fixes

---
+++ pages/dashboard/products/add.tsx
@@ -0,0 +1,12 @@
+import Dashboard from '@/pages/dashboard';
+import * as React from 'react';
+
+const AddProductPage = () => {
+  return (
+    <Dashboard>
+      <div>add new product</div>
+    </Dashboard>
+  );
+};
+
+export default AddProductPage;
